3093318a085283a1e80b44a5081d889969c10d55
akshukisku <akshay.kisku4@gmail.com>
2025-06-29 12:16:34 +0530

    conditonal statement currency breakdown and rating done

---
+++ script.js
@@ -0,0 +1,86 @@
+// currency breakdown
+//  var amount = Number(prompt("Enter the amount"))
+//   let n2000 = 0, n500 = 0, n200 = 0, n100 = 0, n50 = 0;
+//   let n20 = 0, n10 = 0, n5 = 0, n2 = 0, n1 = 0;
+
+//   if (amount >= 2000) {
+//     n2000 = Math.floor(amount / 2000);
+//     amount = amount % 2000;
+//   }
+
+//   if (amount >= 500) {
+//     n500 = Math.floor(amount / 500);
+//     amount = amount % 500;
+//   }
+
+//   if (amount >= 200) {
+//     n200 = Math.floor(amount / 200);
+//     amount = amount % 200;
+//   }
+
+//   if (amount >= 100) {
+//     n100 = Math.floor(amount / 100);
+//     amount = amount % 100;
+//   }
+
+//   if (amount >= 50) {
+//     n50 = Math.floor(amount / 50);
+//     amount = amount % 50;
+//   }
+
+//   if (amount >= 20) {
+//     n20 = Math.floor(amount / 20);
+//     amount = amount % 20;
+//   }
+
+//   if (amount >= 10) {
+//     n10 = Math.floor(amount / 10);
+//     amount = amount % 10;
+//   }
+
+//   if (amount >= 5) {
+//     n5 = Math.floor(amount / 5);
+//     amount = amount % 5;
+//   }
+
+//   if (amount >= 2) {
+//     n2 = Math.floor(amount / 2);
+//     amount = amount % 2;
+//   }
+
+//   if (amount >= 1) {
+//     n1 = amount;
+//   }
+
+//   console.log("₹2000 x", n2000);
+//   console.log("₹500 x", n500);
+//   console.log("₹200 x", n200);
+//   console.log("₹100 x", n100);
+//   console.log("₹50 x", n50);
+//   console.log("₹20 x", n20);
+//   console.log("₹10 x", n10);
+//   console.log("₹5 x", n5);
+//   console.log("₹2 x", n2);
+//   console.log("₹1 x", n1);
+
+//Rating films
+
+// let rating = Number(prompt("Enter the rating"));
+
+// if (rating>0.0 && rating<=2.0){
+//     console.log("Flop");
+// }
+// if (rating>2.1 && rating <=3.4)
+// {
+//     console.log("Semi-hit");
+// }
+// if (rating>3.5 && rating<=4.5)
+// {
+//     console.log("Hit");
+// }
+// if (rating >4.6 && rating <=5.0)
+// {
+//     console.log("BlockBuster");
+// }
+
+
